14776e3f801d58693da8a698ae3eb11656e09499
zbl <zhoubole@ncic.ac.cn>
2018-11-16 19:07:37 +0800

    modified blis test, add mkl test

---
+++ CPU/mkl/benchmark_blis.sh
@@ -0,0 +1,15 @@
+iter_num=10      #repeated test times
+format=NN
+NUM_THREADS=$1  #thread num
+echo "Threads: " ${NUM_THREADS} "iter_num: " ${iter_num}
+
+EXE_BLIS=./gemm_blis
+  echo "blis"
+  export BLIS_NUM_THREADS=${NUM_THREADS}
+  export OMP_PLACES=cores
+  export OMP_PROC_BIND=close
+  ${EXE_BLIS} ${iter_num} ${format} 10000 10000 10000 
+  #for size in {10000..12000..64}
+  #do
+  #  ${EXE_BLIS} ${iter_num} ${format} ${size} ${size} ${size}
+  #done
