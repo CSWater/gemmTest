a91fc5698ebc9bcfbb84340a8ce69f198b7e69c6
CSWater <shuichaoyang@ncic.ac.cn>
2019-03-16 06:14:18 -0400

    fix bugs in lda in nvidia

--- NVIDIA/dgemmTest.cpp
+++ NVIDIA/dgemmTest.cpp
@@ -81,66 +81,66 @@ int main(int argc, char **argv) {
   }
 
   /* Allocate host memory for the matrices */
-  h_A = reinterpret_cast<double *>(malloc(m * k * sizeof(h_A[0])));
+  h_A = reinterpret_cast<double *>(malloc(lda * k * sizeof(h_A[0])));
   if (h_A == 0) {
     fprintf(stderr, "!!!! host memory allocation error (A)\n");
     return EXIT_FAILURE;
   }
 
-  h_B = reinterpret_cast<double *>(malloc(k * n * sizeof(h_B[0])));
+  h_B = reinterpret_cast<double *>(malloc(k * ldb * sizeof(h_B[0])));
   if (h_B == 0) {
     fprintf(stderr, "!!!! host memory allocation error (B)\n");
     return EXIT_FAILURE;
   }
 
-  h_C = reinterpret_cast<double *>(malloc(m * n * sizeof(h_C[0])));
+  h_C = reinterpret_cast<double *>(malloc(ldc * n * sizeof(h_C[0])));
   if (h_C == 0) {
     fprintf(stderr, "!!!! host memory allocation error (C)\n");
     return EXIT_FAILURE;
   }
 
   /* Fill the matrices with test data */
-  for (i = 0; i < m * k; i++) {
+  for (i = 0; i < lda * k; i++) {
     h_A[i] = rand() / static_cast<double>(RAND_MAX);
   }
-  for (i = 0; i < k * n; i++) {
+  for (i = 0; i < k * ldb; i++) {
     h_B[i] = rand() / static_cast<double>(RAND_MAX);
   }
-  for (i = 0; i < m * n; i++) {
+  for (i = 0; i < lda * ldb; i++) {
     h_C[i] = rand() / static_cast<double>(RAND_MAX);
   }
 
   /* Allocate device memory for the matrices */
-  if (cudaMalloc(reinterpret_cast<void **>(&d_A), m * k * sizeof(d_A[0])) !=
+  if (cudaMalloc(reinterpret_cast<void **>(&d_A), lda * k * sizeof(d_A[0])) !=
       cudaSuccess) {
     fprintf(stderr, "!!!! device memory allocation error (allocate A)\n");
     return EXIT_FAILURE;
   }
-  if (cudaMalloc(reinterpret_cast<void **>(&d_B), n * k * sizeof(d_B[0])) !=
+  if (cudaMalloc(reinterpret_cast<void **>(&d_B), ldb * k * sizeof(d_B[0])) !=
       cudaSuccess) {
     fprintf(stderr, "!!!! device memory allocation error (allocate B)\n");
     return EXIT_FAILURE;
   }
-  if (cudaMalloc(reinterpret_cast<void **>(&d_C), m * n * sizeof(d_C[0])) !=
+  if (cudaMalloc(reinterpret_cast<void **>(&d_C), ldc * n * sizeof(d_C[0])) !=
       cudaSuccess) {
     fprintf(stderr, "!!!! device memory allocation error (allocate C)\n");
     return EXIT_FAILURE;
   }
 
   /* Initialize the device matrices with the host matrices */
-  status = cublasSetVector(m * k, sizeof(h_A[0]), h_A, 1, d_A, 1);
+  status = cublasSetVector(lda * k, sizeof(h_A[0]), h_A, 1, d_A, 1);
   if (status != CUBLAS_STATUS_SUCCESS) {
     fprintf(stderr, "!!!! device access error (write A)\n");
     return EXIT_FAILURE;
   }
 
-  status = cublasSetVector(n * k, sizeof(h_B[0]), h_B, 1, d_B, 1);
+  status = cublasSetVector(ldb * k, sizeof(h_B[0]), h_B, 1, d_B, 1);
   if (status != CUBLAS_STATUS_SUCCESS) {
     fprintf(stderr, "!!!! device access error (write B)\n");
     return EXIT_FAILURE;
   }
 
-  status = cublasSetVector(m * n, sizeof(h_C[0]), h_C, 1, d_C, 1);
+  status = cublasSetVector(ldc * n, sizeof(h_C[0]), h_C, 1, d_C, 1);
   if (status != CUBLAS_STATUS_SUCCESS) {
     fprintf(stderr, "!!!! device access error (write C)\n");
     return EXIT_FAILURE;
